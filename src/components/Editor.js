d028af75ee1933129831c4c5a660a6674037df22
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-06-07 16:29:55 +0530

    1 file modified

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,28 +1,26 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
-import {FontAwesomeIcon} from'@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Editor(props) {
   const { language, displayName, value, onchange, className } = props;
-  const [open,setOpen] = useState(true);
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onchange(value);
   }
   return (
-    <div className={`editor-container${open? '':'collapsed'}`}>
+    <div className={`editor-container${open ? '' : 'collapsed'}`}>
       <div className="editor-title">
         {displayName}
-        <button 
-        onClick={()=> setOpen(prevOpen =>!prevOpen)}
-        >
-          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt } />
+        <button onClick={() => setOpen((prevOpen) => !prevOpen)}>
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
         </button>
       </div>
       <ControlledEditor
